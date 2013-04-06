aceb9e7ecc33b6d6413d2279bbf269f18a5f6637
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 20:06:34 -0500

    p0

--- src/edu/uchicago/cs/MyBrowser.java
+++ src/edu/uchicago/cs/MyBrowser.java
@@ -9,46 +9,21 @@ import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MyBrowser extends Activity implements OnKeyListener {
+public class MyBrowser extends Activity  {
  	
-	//TextView txt;
-	WebView web;
-	EditText edt;
+
 	
 	
 	@Override			
  	public void onCreate(Bundle savedInstanceState) {
  		super.onCreate(savedInstanceState);
  		setContentView(R.layout.mybrowser);
- 		
- 		//txt = (TextView) findViewById(R.id.txt);
- 		edt = (EditText) findViewById(R.id.edt);
- 		edt.setOnKeyListener(this);
- 		
- 		web = (WebView) findViewById(R.id.web);
 
- 		//the getData is the URI that you passed into the intent. 
- 		String strUrl = getIntent().getData().toString();
- 		edt.setText(strUrl);
- 		web.loadUrl(strUrl);
  		
 
  		
  	}
 
 
-	@Override
-	public boolean onKey(View v, int keyCode, KeyEvent event) {
-		// TODO Auto-generated method stub
-		
-		if (keyCode ==  KeyEvent.KEYCODE_ENTER ){
-			web.loadUrl(edt.getText().toString());
-			return true;
-		}
-		else {
-			return false;
-		}
-
-	}
 
 }
